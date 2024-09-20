c7ea60ca5a25e09df90640b3d1682133fc26e793
merty <mertyigit148@gmail.com>
2024-09-20 22:42:42 +0300

    v1.4.0 Coroutines used for splash.

--- app/src/main/java/com/mertyigit0/vocabcards/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/mertyigit0/vocabcards/ui/splash/SplashActivity.kt
@@ -4,13 +4,16 @@ import android.animation.Animator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.mertyigit0.vocabcards.R
 import com.mertyigit0.vocabcards.databinding.ActivitySplashBinding
 import com.mertyigit0.vocabcards.ui.main.MainActivity
 import com.mertyigit0.vocabcards.ui.onboarding.OnboardingActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -33,7 +36,9 @@ class SplashActivity : AppCompatActivity() {
 
         val splashScreenDuration = 3000L // 3 saniye
 
-        Handler().postDelayed({
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashScreenDuration)
+
             val intent = if (isOnboardingCompleted) {
                 Intent(this@SplashActivity, MainActivity::class.java)
             } else {
@@ -41,7 +46,9 @@ class SplashActivity : AppCompatActivity() {
             }
             startActivity(intent)
             finish()
-        }, splashScreenDuration)
+        }
+
+
 
 
         lottieAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
