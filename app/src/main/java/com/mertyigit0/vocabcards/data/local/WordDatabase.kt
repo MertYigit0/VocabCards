2006c52200d73ee6016f2a22a076693d4d8e00a1
merty <mertyigit148@gmail.com>
2024-09-21 13:00:01 +0300

    v1.5.3 words update.

--- app/src/main/java/com/mertyigit0/vocabcards/data/local/WordDatabase.kt
+++ app/src/main/java/com/mertyigit0/vocabcards/data/local/WordDatabase.kt
@@ -8,7 +8,7 @@ import com.mertyigit0.vocabcards.data.model.CategoryDao
 import com.mertyigit0.vocabcards.data.model.Word
 import com.mertyigit0.vocabcards.data.model.WordDao
 
-@Database(entities = [Word::class, Category::class], version = 4, exportSchema = false)
+@Database(entities = [Word::class, Category::class], version = 1, exportSchema = false)
 abstract class WordDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
     abstract fun categoryDao(): CategoryDao
