74e51815dbd88e942fe9b47164cc4a1a45a3ae20
merty <mertyigit148@gmail.com>
2024-10-10 02:49:57 +0300

    1.8.4 release version 10,new sport words.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 10
-        versionName = "1.8.3"
+        versionName = "1.8.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
