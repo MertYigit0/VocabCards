7bde76e6de29a7f951029b19e48a5cba1d353003
merty <mertyigit148@gmail.com>
2024-10-13 06:57:05 +0300

    1.9.1 version 14 added new category.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mertyigit0.vocabcards"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.8.7"
+        versionCode = 14
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
