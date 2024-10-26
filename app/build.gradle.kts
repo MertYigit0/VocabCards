97938eeaf4866beaa5ce58e41614a196cfa8529e
merty <mertyigit148@gmail.com>
2024-10-26 09:52:01 +0300

    2.0.0 version 16 prod.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mertyigit0.vocabcards"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.9.1"
+        versionCode = 16
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
